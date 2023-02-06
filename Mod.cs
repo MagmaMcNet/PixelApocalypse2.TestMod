c164ff48005e772111a6732deda5c4b27c196629
SMLkaiellis08 <kaicycle11@gmail.com>
2023-02-06 21:35:56 +0000

    Add project files.

---
+++ Mod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MenuUIMod
+{
+    public class Mod
+    {
+        public void Initialize()
+        {
+            Debug.Log("Menu UI Test Mod Loaded By MagmaMc");
+
+        }
+        // Called Every 100ms 
+        public void ModUpdate()
+        {
+
+
+            
+        }
+    }
+}
